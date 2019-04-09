d7ad3cdd2af167e7e0cc200282dd122f6e514ca4
David Johnson-Davies <david@interface.co.uk>
2019-04-09 10:50:31 +0100

    Version 3 - 9th April 2019
    Added prices from rs-online

--- attiny-database.lisp
+++ attiny-database.lisp
@@ -1,5 +1,5 @@
-; The ATtiny database v2 - 6th April 2019
-; with 1-off prices (in pence) from https://uk.farnell.com/
+; The ATtiny database v3 - 9th April 2019
+; with 1-off prices (in pence) from https://uk.farnell.com/ and https://uk.rs-online.com/
 
 (defvar *data*
   '((attinyX5 (pins 8) (io 5) (adc 4) (pwm 3) (usi 1) (timer8 2) (crystal) (pll))
@@ -27,20 +27,20 @@
 
     (attinyX7 (pins 20) (io 16) (adc 11) (pwm 3) (uart 1) (usi 1) (timer8 1) (timer16 1) (crystal) (lin))
     (attiny167 (family attinyX7) (flash 16384) (ram 512) (eeprom 512) (price soic 111))
-    (attiny87 (family attinyX7) (flash 8192) (ram 512) (eeprom 512))
+    (attiny87 (family attinyX7) (flash 8192) (ram 512) (eeprom 512) (price soic 124))
 
     (attinyX8 (pins 28) (io 27) (adc 8) (pwm 4) (i2c master) (i2c slave) (usi 1) (timer8 1) (timer16 1))
     (attiny88 (family attinyX8) (flash 8192) (ram 512) (eeprom 64) (price tqfp 76) (price pdip 143))
     (attiny48 (family attinyX8) (flash 4096) (ram 256) (eeprom 64) (price tqfp 78) (price pdip 131))
     
     (attinyX34 (pins 20) (io 17) (adc 12) (pwm 4) (i2c slave) (uart 2) (usi 1) (timer8 1) (timer16 1) (crystal))
-    (attiny1634 (family attinyX34) (flash 16384) (ram 1024) (eeprom 256) (price tqfp 116))
+    (attiny1634 (family attinyX34) (flash 16384) (ram 1024) (eeprom 256) (price soic 118))
 
     (attinyX28 (pins 32) (io 27) (adc 28) (pwm 4) (i2c slave) (uart 1) (timer8 1) (timer16 1))
     (attiny828 (family attinyX28) (flash 8192) (ram 512) (eeprom 512) (price tqfp 84))
 
     (attinyX3 (pins 20) (io 15) (adc 4) (pwm 4) (usi 1) (timer8 1) (timer16 1) (boost))
-    (attiny43 (family attinyX3) (flash 4096) (ram 512) (eeprom 64))
+    (attiny43 (family attinyX3) (flash 4096) (ram 512) (eeprom 64) (price soic 147))
     
     (attiny9/10 (pins 6) (io 4) (adc 4) (pwm 2) (timer16 1))
     (attiny4/5 (pins 6) (io 4) (pwm 2) (timer16 1))
